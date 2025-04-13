7de6cb193af82af072bbbf2f65026919ac5c3caf
sumu <2038035593@qq.com>
2025-04-13 23:46:03 +0800

    kernel执行reboot的时候支持卸载驱动

--- rootfs_custom/firmware/initial.sh
+++ rootfs_custom/firmware/initial.sh
@@ -2,5 +2,22 @@
 
 cd /firmware/modules
 
-echo "load driver modules..."
-insmod sdriver_revision.ko
+case "$1" in
+  start)
+        echo "load driver modules ..."
+        insmod sdriver_revision.ko
+        ;;
+  stop)
+        echo "unload driver modules ..."
+        rmmod sdriver_revision.ko
+        ;;
+  restart|reload)
+        "$0" stop
+        "$0" start
+        ;;
+  *)
+        echo "Usage: $0 {start|stop|restart}"
+        exit 1
+esac
+
+exit $?
